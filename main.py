213b5cf6c8e1716da99ae2e075cdc45ca0b9e6fa
zaryab-ali <zaryabali768@gmail.com>
2023-04-13 02:49:19 +0500

    added lyrics first time

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ from pathlib import Path
 import os
 from flask import Flask,request
 
+import speech_recognition as sr
+from pydub import AudioSegment
+from pydub.utils import make_chunks
+
+
 
 
 app = Flask(__name__)
@@ -22,7 +27,7 @@ def shit21():
     return f"yes u are shit"
 
 
-@app.route('/api/foo/', methods=['GET'])
+@app.route('/api/plag/', methods=['GET'])
 def foo(out_file1="audio1.mp3", out_file2="audio2.mp3"):
   bar = request.args.to_dict()
   print(bar)
@@ -68,6 +73,56 @@ def foo(out_file1="audio1.mp3", out_file2="audio2.mp3"):
   print(r)
   return r
 
+
+@app.route("api/audiotolyrics/<path:url>")
+def download(url, out_file="audio.mp3"):
+  big = []
+  text = ""
+  #out_file = Path(f"/content/{out_file}").expanduser()
+
+  resp = requests.get(url)
+  resp.raise_for_status()
+  with open("audio.mp3", "wb") as fout:
+    fout.write(resp.content)
+  sound = AudioSegment.from_mp3("audio.mp3")
+  sound.export("blindingLights.wav", format="wav")
+  filename = "blindingLights.wav"
+  # os.mkdir("/content")
+  os.mkdir("temp")
+  myaudio = AudioSegment.from_wav(filename)
+  chunk_lenght_ms = 5000
+  chunks = make_chunks(myaudio, chunk_lenght_ms)
+  for i, chunk in enumerate(chunks):
+    chunkName = 'temp/' + filename + "_{0}.wav".format(i)
+    chunk.export(chunkName, format="wav")
+
+  for files in sorted(os.listdir("temp")):
+    filesss = "temp/" + files
+    print(files)
+    r = sr.Recognizer()
+
+    # open the file
+    with sr.AudioFile(filesss) as source:
+      # listen for the data (load audio to memory)
+      audio_data = r.record(source)
+      # recognize (convert from speech to text)
+      try:
+        text = r.recognize_google(audio_data)
+        print(text)
+      except:
+        print("...")
+        text = "..."
+    # small = [text]
+    big.append(text)
+  os.remove("audio.mp3")
+  os.remove("blindingLights.wav")
+  for files in sorted(os.listdir("temp")):
+    filesss = "temp/" + files
+    os.remove(filesss)
+  os.rmdir("temp")
+  # j = {"lyrics" : big}
+  print(big)
+  return big
 
 
 if __name__ == '__main__':
