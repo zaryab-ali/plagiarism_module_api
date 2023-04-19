7bc4493eb9d815729c36f6ca0915cef9646654a4
zaryab-ali <zaryabali768@gmail.com>
2023-04-19 09:53:32 +0500

    shit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 
 
 import hashlib
+import json
+import pickle
+import numpy as np
+
 from cdifflib import CSequenceMatcher
 import requests
 from pathlib import Path
@@ -10,6 +14,8 @@ from flask import Flask,request
 import speech_recognition as sr
 from pydub import AudioSegment
 from pydub.utils import make_chunks
+import ffmpeg
+import ffprobe
 
 
 
@@ -74,60 +80,79 @@ def foo(out_file1="audio1.mp3", out_file2="audio2.mp3"):
   return r
 
 
-@app.route("/api/audiotolyrics/", methods=['GET'])
-def lyrics(out_file="audio.mp3"):
-  bar = request.args.to_dict()
-  print(bar)
-  url3 = bar.get("a")
-  
-  big = []
-  text = ""
-  #out_file = Path(f"/content/{out_file}").expanduser()
+# @app.route("/api/audiotolyrics/", methods=['GET'])
+# def lyrics(out_file="audio.mp3"):
+#   bar = request.args.to_dict()
+#   print(bar)
+#   url3 = bar.get("a")
+#
+#   big = []
+#   text = ""
+#   #out_file = Path(f"/content/{out_file}").expanduser()
+#
+#   resp = requests.get(url3)
+#   resp.raise_for_status()
+#   with open("audio.mp3", "wb") as fout:
+#     fout.write(resp.content)
+#   sound = AudioSegment.from_mp3("audio.mp3")
+#   sound.export("blindingLights.wav", format="wav")
+#   filename = "blindingLights.wav"
+#   # os.mkdir("/content")
+#   os.mkdir("temp")
+#   myaudio = AudioSegment.from_wav(filename)
+#   chunk_lenght_ms = 5000
+#   chunks = make_chunks(myaudio, chunk_lenght_ms)
+#   for i, chunk in enumerate(chunks):
+#     chunkName = 'temp/' + filename + "_{0}.wav".format(i)
+#     chunk.export(chunkName, format="wav")
+#
+#   for files in sorted(os.listdir("temp")):
+#     filesss = "temp/" + files
+#     print(files)
+#     r = sr.Recognizer()
+#
+#     # open the file
+#     with sr.AudioFile(filesss) as source:
+#       # listen for the data (load audio to memory)
+#       audio_data = r.record(source)
+#       # recognize (convert from speech to text)
+#       try:
+#         text = r.recognize_google(audio_data)
+#         print(text)
+#       except:
+#         print("...")
+#         text = "..."
+#     # small = [text]
+#     big.append(text)
+#   os.remove("audio.mp3")
+#   os.remove("blindingLights.wav")
+#   for files in sorted(os.listdir("temp")):
+#     filesss = "temp/" + files
+#     os.remove(filesss)
+#   os.rmdir("temp")
+#   # j = {"lyrics" : big}
+#   print(big)
+#   return big
+#
 
-  resp = requests.get(url3)
-  resp.raise_for_status()
-  with open("audio.mp3", "wb") as fout:
-    fout.write(resp.content)
-  sound = AudioSegment.from_mp3("audio.mp3")
-  sound.export("blindingLights.wav", format="wav")
-  filename = "blindingLights.wav"
-  # os.mkdir("/content")
-  os.mkdir("temp")
-  myaudio = AudioSegment.from_wav(filename)
-  chunk_lenght_ms = 5000
-  chunks = make_chunks(myaudio, chunk_lenght_ms)
-  for i, chunk in enumerate(chunks):
-    chunkName = 'temp/' + filename + "_{0}.wav".format(i)
-    chunk.export(chunkName, format="wav")
+@app.route("/genre")
+def find_genre():
+  loaded_mod = pickle.load(open('song_prediction.pickle', 'rb'))
+  with open('testing.json', "r") as fp:
+    data = json.load(fp)
 
-  for files in sorted(os.listdir("temp")):
-    filesss = "temp/" + files
-    print(files)
-    r = sr.Recognizer()
+  X = np.array(data["mfcc"])
 
-    # open the file
-    with sr.AudioFile(filesss) as source:
-      # listen for the data (load audio to memory)
-      audio_data = r.record(source)
-      # recognize (convert from speech to text)
-      try:
-        text = r.recognize_google(audio_data)
-        print(text)
-      except:
-        print("...")
-        text = "..."
-    # small = [text]
-    big.append(text)
-  os.remove("audio.mp3")
-  os.remove("blindingLights.wav")
-  for files in sorted(os.listdir("temp")):
-    filesss = "temp/" + files
-    os.remove(filesss)
-  os.rmdir("temp")
-  # j = {"lyrics" : big}
-  print(big)
-  return big
+  prediction = loaded_mod.predict(X)
 
+  print(prediction)
+
+  predicted_index = np.argmax(prediction, axis=1)
+
+  print("******************************************************************")
+  print(predicted_index[0])
+
+  return predicted_index[0]
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0",port=5000,debug=True)
